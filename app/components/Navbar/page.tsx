27ccda6f15038c79b2b88503c9b9faaa0276cfcc
Isaac Wong <47isaacw@gmail.com>
2024-08-12 21:28:28 -0400

    home page started

---
+++ app/components/Navbar/page.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+	return (
+		<section className={'border-b-2 border-hack-gray py-2'}>
+			<a href="https://hackclub.com/"><img className={'w-24'} src="https://assets.hackclub.com/flag-orpheus-top.svg" alt="Hack Club"/></a>
+
+		</section>
+	)
+}
